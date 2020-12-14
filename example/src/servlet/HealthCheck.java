fff3d040a5b86829b5ca96511cc58ab022474f26
Yaling <huangyl0723@gmail.com>
2020-12-14 17:28:09 +0900

    workspace

--- example/src/servlet/HealthCheck.java
+++ example/src/servlet/HealthCheck.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import model.Health;
 import model.HealthCheckLogic;
 
+@WebServlet("/HealthCheck")
 public class HealthCheck extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
